58f09eca2fe9e837ca52255a6a57649e2b6c0294
Mohammad Zrar <105074619+mohammad-zrar@users.noreply.github.com>
2023-01-17 12:43:52 +0300

    Set the variable environment

--- send_hb.py
+++ send_hb.py
@@ -5,10 +5,11 @@ import pandas
 import smtplib
 import random
 import datetime as dt
+import os
 
 PLACEHOLDER = "[NAME]"
-MY_EMAIL = "mohammad44zrar@gmail.com"
-MY_PASSWORD = "qmzcqsfdavqepika"
+MY_EMAIL = os.environ.get("MY_EMAIL")
+MY_PASSWORD = os.environ.get("MY_PASSWORD")
 
 
 # 1. Update the birthdays.csv
@@ -19,22 +20,22 @@ info = data.to_dict(orient="records")
 today = dt.datetime.now()
 month = today.month
 day = today.day
-
-for person in info:
-    if person["month"] == month and person["day"] == day:
-        letter_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-        with open(letter_path) as letter:
-            letter_content = letter.read()
-            new_letter = letter_content.replace(PLACEHOLDER, person["name"])
-            message = f"subject:Happy birthday\n\n{new_letter}"
-            with smtplib.SMTP("smtp.gmail.com", 587) as connection:
-                connection.starttls()
-                connection.login(user=MY_EMAIL, password=MY_PASSWORD)
-                connection.sendmail(
-                    from_addr=MY_EMAIL,
-                    to_addrs=person["email"],
-                    msg=message
-                )
+#
+# for person in info:
+#     if person["month"] == month and person["day"] == day:
+#         letter_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
+#         with open(letter_path) as letter:
+#             letter_content = letter.read()
+#             new_letter = letter_content.replace(PLACEHOLDER, person["name"])
+#             message = f"subject:Happy birthday\n\n{new_letter}"
+#             with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+#                 connection.starttls()
+#                 connection.login(user=MY_EMAIL, password=MY_PASSWORD)
+#                 connection.sendmail(
+#                     from_addr=MY_EMAIL,
+#                     to_addrs=person["email"],
+#                     msg=message
+#                 )
 
 # 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME]
 # with the person's actual name from birthdays.csv
